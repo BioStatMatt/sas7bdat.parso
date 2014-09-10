4434555107cac057e9f6b4d56d5da0ecd0ef3b79
Matthew S. Shotwell <biostatmatt@gmail.com>
2014-09-10 12:27:49 -0500

    utilize readAll methods of SasFileReader

--- java/s7b2csvclass.java
+++ java/s7b2csvclass.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.lang.Math;
 
 public class s7b2csvclass  {
   public String s7b2csv(String s7bfile, String csvfile) {
@@ -14,25 +15,40 @@ public class s7b2csvclass  {
       sin = new FileInputStream(s7bfile);
       sout = new FileWriter(csvfile);
 
+      // create reader and get file properties
       SasFileReader sfr = new SasFileReader(sin);
       SasFileProperties sfp = sfr.getSasFileProperties();
+
+      // compute data size
+      long dsz = sfp.getRowCount() * sfp.getRowLength(); 
+
+      // create CSV data writer
       CSVDataWriter cdw = new CSVDataWriter(sout);  
 
       // write column headers
       List<Column> col = sfr.getColumns();
       cdw.writeColumnNames(col);
-
-      // write data
-      for(long row = 0; row < sfp.getRowCount(); row++)
-        cdw.writeRow(col, sfr.readNext()); 
+      
+      // sequential read/write for data size > 2^28 (0.25GB)
+      if(dsz > (long) Math.pow(2,28)) {
+        // sequential read/write
+        for(long row = 0; row < sfp.getRowCount(); row++)
+          cdw.writeRow(col, sfr.readNext()); 
+      } else {
+        // simultaneous read/write
+        cdw.writeRowsArray(col, sfr.readAll());
+      }
  
+      // close file streams
       sin.close();
       sout.close();
 
+      // return exception text if thrown 
       } catch(IOException e) {
         return(e.getMessage());
       } 
 
+    // return CSV filename on success
     return(csvfile);
   }
 } 
