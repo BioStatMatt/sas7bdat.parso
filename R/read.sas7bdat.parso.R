5013663587f18584011f26888e28bf1dbc0dae73
ZJ <zhuojia.dai@gmail.com>
2015-02-05 12:54:29 +1100

    Update read.sas7bdat.parso.R

--- R/read.sas7bdat.parso.R
+++ R/read.sas7bdat.parso.R
@@ -1,2 +1,7 @@
-read.sas7bdat.parso <- function(file, ...)
-  read.csv(s7b2csv(file, tempfile()), ...)
+#' Read a SAS dataset (sas7bdat format)
+#' @param
+read.sas7bdat.parso <- function(file, ..., READ_FUNC = read.csv)
+  READ_FUNC(s7b2csv(file, tempfile()), ...)
+
+#
+fread
