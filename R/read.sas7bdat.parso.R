2a527db5db4d868ee9f6dbf8231e3e65f8f7f0a3
rstudio <rstudio@example.com>
2015-02-05 02:16:32 +0000

    more flexible read.sas7bdat.parso function by allowing for a custom specification of the read function e.g. allow fread to be used instead of read.csv.
    Also added a fread.sas7bdat.parso

--- R/read.sas7bdat.parso.R
+++ R/read.sas7bdat.parso.R
@@ -1,7 +1,14 @@
 #' Read a SAS dataset (sas7bdat format)
-#' @param
+#' @param file The SAS dataset file that you wish to read in
+#' @param READ_FUNC The function to used to read in the intermediate csv file. 
 read.sas7bdat.parso <- function(file, ..., READ_FUNC = read.csv)
   READ_FUNC(s7b2csv(file, tempfile()), ...)
 
-#
-fread
+#' Read a SAS dataset (sas7bdat format) using data.table's fread
+#' @param file The SAS dataset file that you wish to read in
+#' @param READ_FUNC The function to used to read in the intermediate csv file. 
+#' @import data.table
+#' @return data.table object
+fread.sas7bdat.parso <- function(file, ...) {
+  read.sas7bdat.parso(file,..., READ_FUNC = fread)
+}
