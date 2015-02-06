867f26a2ffdc87f16ec1856105804cdd98f99edd
Matthew S. Shotwell <matt.shotwell@vanderbilt.edu>
2015-02-06 10:05:56 -0600

    quiet R CMD check warnings

--- R/read.sas7bdat.parso.R
+++ R/read.sas7bdat.parso.R
@@ -1,7 +1,7 @@
 #' Read a SAS dataset (sas7bdat format)
 #' @param file The SAS dataset file that you wish to read in
 #' @param READ_FUNC The function to used to read in the intermediate csv file. 
-read.sas7bdat.parso <- function(file, ..., READ_FUNC = read.csv)
+read.sas7bdat.parso <- function(file, READ_FUNC = read.csv, ...)
   READ_FUNC(s7b2csv(file, tempfile()), ...)
 
 #' Read a SAS dataset (sas7bdat format) using data.table's fread
